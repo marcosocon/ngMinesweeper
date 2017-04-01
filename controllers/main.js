019a4062ed2dfeb2873bb9f182d77216b666edbe
marcosocon <oconmarcos97@gmail.com>
2017-03-31 23:13:25 -0300

    project set up

---
+++ controllers/main.js
@@ -0,0 +1,5 @@
+angular.module('ngMinesweeper')
+    .controller('MainCtrl', function ($scope, _) {
+        var self = this;
+        
+    });
---
+++ services/lodash.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('lodash', [])
+  .factory('_', ['$window', function($window) {
+    return $window._; // assumes lodash has already been loaded on the page.
+}]);
